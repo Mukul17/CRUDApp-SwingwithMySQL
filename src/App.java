5a535e5936b22c305a37593a8718a42c52162673
Mukul Kopulwar <mukulkopulwar71@gmail.com>
2022-09-08 15:13:11 +0530

    initial commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame jframe = new JFrame("Hello World");
+		jframe.setVisible(true);
+		jframe.setSize(500,600);
+		
+
+	}
+
+}
